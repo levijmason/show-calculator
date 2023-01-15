5b9523cbfb8b07bc9a8e4bf017f5b0fd6396a9c1
levijmason <levijackmason@gmail.com>
2023-01-15 12:35:03 -0600

    initial pages update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,35 @@
-import '@/styles/globals.css'
+import { Layout } from '@/components/common/Layout'
+import { en_messages } from '@/languages/en'
+import { darkTheme, lightTheme } from '@/styles'
+import { Fonts } from '@/styles/Fonts'
+import { NextUIProvider } from '@nextui-org/react'
+import { SSRProvider } from '@react-aria/ssr'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
+import { IntlProvider } from 'react-intl'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function ShowCalculator({ Component, pageProps }: AppProps) {
+  return (
+    <SSRProvider>
+      <IntlProvider locale={'en-US'} messages={en_messages}>
+        <NextThemesProvider
+          defaultTheme="dark"
+          attribute="class"
+          value={{
+            light: lightTheme.className,
+            dark: darkTheme.className,
+          }}
+        >
+          <NextUIProvider>
+            <Fonts />
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </NextUIProvider>
+        </NextThemesProvider>
+      </IntlProvider>
+    </SSRProvider>
+  )
 }
+
+export default ShowCalculator
