2b9b611d5d27b1bd39bdb81f6c2c721ff79afe1b
levijmason <levijackmason@gmail.com>
2023-01-16 01:44:35 -0600

    add en messages

--- languages/en.ts
+++ languages/en.ts
@@ -1,16 +1,19 @@
 export const en_messages = {
   appTitle: 'Show Calculator ',
-  appInstructions: 'Input the info for your show below to see the potential gross and breakeven point!',
   createdBy: ' by Trip the Fan',
   navButton: 'Reach Out',
 
-  potentialGross: 'Potential Gross: ',
-  totalExpenses: ' Total Expenses: ',
-  breakEven: 'Break Even: ',
+  fieldRequired: 'This field is required',
+
+  potentialGross: 'Potential Gross ',
+  totalExpenses: 'Total Expenses ',
+  breakEven: 'Break Even ',
+  potentialNet: 'Potential Net ',
+  riskRating: 'Risk Rating',
 
   venueCapacity: 'Venue capacity',
   averageTicketPrice: 'Average ticket price',
-  artistPay: 'Artist pay',
+  artistPay: 'Artist guarantee',
   productionCost: 'Production cost',
   marketingCost: 'Marketing cost',
   otherCost: 'Other cost',
