e57981f5f8daada7eaa5bfc83adb15916cc3f108
levijmason <levijackmason@gmail.com>
2023-01-29 22:24:08 -0600

    update meta description

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Show Calculator</title>
         <meta
           name="description"
-          content="Trip the Fan is a music management firm based out of Oklahoma City."
+          content="Show Calculator is an app to help visualize the numbers behind a show proposal"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
